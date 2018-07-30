2947fbdf36219b0601614ff4cf0c7d27f9ebacf3
PME\mgileau <mgileau@prattmiller.com>
2018-07-30 08:32:00 -0400

    no message

--- testingGITfiltering.m
+++ testingGITfiltering.m
@@ -5,8 +5,5 @@ c = a + b;
 if c == 3
    disp('The sum is three');
    disp('Try and ignore this line')
-   b = a; %gitignore
-    b = a*b; %gitignore
-    c = b*a; 
-    buuts = c; %gitignore
+    buuts = c; 
 end
